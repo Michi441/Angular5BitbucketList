210828c0e92d67e96bf376ff3704885f495d2bf7
Doris <DorisMac@Doriss-MBP.local>
2018-04-26 17:32:52 +0200

    Added active router

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,14 +8,22 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
   animations: [
     trigger('goals', [
       transition('* => *', [
-        query(':enter', style({ opacity: 0}), {optional: true})
+        query(':enter', style({ opacity: 0}), {optional: true}),
 
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
             style({opacity: 1, transform: 'translateY(0)', offset: 1}),
-          ]))]), {optional: true})
+          ]))]), {optional: true}),
+
+
+          query(':leave', stagger('300ms', [
+            animate('.6s ease-in', keyframes([
+              style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+              style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+              style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+            ]))]), {optional: true})
       ])
     ])
   ]
@@ -27,6 +35,7 @@ export class HomeComponent implements OnInit {
   goalText: string = '';
   goals = [];
 
+
   constructor() { }
 
   ngOnInit() {
@@ -37,6 +46,10 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 
 }
